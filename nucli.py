a298993d526d25ff8df2a40f8bff309d15d93044
Mike Pfeiffer <pfeiffermj@outlook.com>
2019-12-15 15:16:30 -0600

    Makes end optional parameter for ping-sweep

--- nucli.py
+++ nucli.py
@@ -9,15 +9,15 @@ def nucli():
 
 @click.command()
 @click.option('--start', help='Starting IP Address')
-@click.option('--end', help='Ending IP Address')
-@click.option('--iface', default= '', help='Source Interface')
+@click.option('--end', default = '', help='(optional) Ending IP Address')
+@click.option('--iface', default= '', help='(optional) Source Interface')
 def ping_sweep(start, end, iface):
     PingTools.ping_range(start, end, iface)
 
 nucli.add_command(ping_sweep)
 
 def click_entry():
-    """Entry Point for Setuptools Integration with Click."""
+    '''Entry Point for Setuptools Integration with Click.'''
     nucli()
 
 if __name__ == '__main__':
