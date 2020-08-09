53f5ea03d7bc5f0867ae67a3b9c8a9f1cc819eff
Mike Pfeiffer <pfeiffermj@outlook.com>
2020-08-09 13:42:10 +0000

    adds a simple ping test

---
+++ tests/test_ping_tools.py
@@ -0,0 +1,19 @@
+  
+import unittest
+from context import app
+
+LOOPBACK = "127.0.0.1"
+
+
+class TestPingTools(unittest.TestCase):
+
+    def test_ping(self):
+        """
+        """
+        pt = app.PingTools()
+        result = pt.ping(LOOPBACK, "")
+        self.assertTrue(result)
+
+
+if __name__ == "__main__":
+    unittest.main()
